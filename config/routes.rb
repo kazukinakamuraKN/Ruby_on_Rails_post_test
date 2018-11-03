f1582e25a5e1fee319b55ff169e0f8833c9a3ada
kazukinakamura <kajirikajiri@gmail.com>
2018-11-03 07:46:51 +0000

    loofah 2.2.3 (was 2.2.2) update!

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  get 'books/index'
+
+  get 'books/create'
+
+  get 'books/new', as: 'new_book'
+
+  get 'books/edit'
+
+  get 'books/show'
+
+  get 'books/update'
+
+  get 'books/destroy'
+  
+  root 'books#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20181008062021_create_books.rb
@@ -0,0 +1,10 @@
+class CreateBooks < ActiveRecord::Migration[5.1]
+  def change
+    create_table :books do |t|
+      t.string :subject
+      t.integer :price
+
+      t.timestamps
+    end
+  end
+end
